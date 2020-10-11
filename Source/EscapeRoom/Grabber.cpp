ce6463850a8d63bb1e95c90e93fcf9637cf2bc3a
Susanna Nevalainen <susanna.nevalainen@oysterlab.ch>
2020-10-11 15:41:43 +0200

    Grabber current rotation and location

--- Source/EscapeRoom/Grabber.cpp
+++ Source/EscapeRoom/Grabber.cpp
@@ -1,6 +1,10 @@
 // Copyright zuzze.tech 2020
-
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 #include "Grabber.h"
+
+// this allows us to improve readability to show what is outparameter
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -8,8 +12,6 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 // Called when the game starts
@@ -23,4 +25,20 @@ void UGrabber::BeginPlay()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get player's view point
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation);
+
+	// Log user location and rotation
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"),
+		   *PlayerViewPointLocation.ToString(),
+		   *PlayerViewPointRotation.ToString());
+
+	// Ray cast to certain distance (Reach)
+
+	// See what the raycaster hits (what we can grab)
 }
